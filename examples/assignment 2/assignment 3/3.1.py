685238351688edee82b674ca62a8d65c8e982562
ag2409 <ag2409@student.jamk.fi>
2024-09-16 15:15:22 +0600

    commit

--- examples/assignment 2/assignment 3/3.1.py
+++ examples/assignment 2/assignment 3/3.1.py
@@ -1,1 +1,14 @@
+age = int(input("Enter your age: "))
+
+if age < 13:
+    print ("child")
+
+elif 13 <= age <=19:
+    print("teen")
+
+elif 20 <= age <= 65:
+    print("adult")
+
+else:
+    print("senior")
 
